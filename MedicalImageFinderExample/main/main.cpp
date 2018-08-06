1c9190962ea4949b93f9fd573c46466afa7ce42d
wuzhuobin <jiejin2022@163.com>
2018-08-06 11:34:12 +0800

    Adding example.

--- MedicalImageFinderExample/main/main.cpp
+++ MedicalImageFinderExample/main/main.cpp
@@ -6,8 +6,12 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	QString path;
+	if (argc > 1) {
+		path = QString::fromStdString(argv[1]);
+	}
 	//RegistrationWizard rw("E:/TestProgram/git/RegistrationWizard/TestImage", 2);
-	MedicalImageFinder rw("E:/TestProgram/git/RegistrationWizard/TestImage", 2);
+	MedicalImageFinder rw(path, 2);
 	rw.setImageModalityNames(0, "T2 image ");
 	rw.setImageModalityNames(1, "MRA image");
 
